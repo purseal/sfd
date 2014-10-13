58e6837a876b9cf06f0feb22f593c258d914cf1a
Maria <korevmaria@gmail.com>
2014-10-14 01:47:46 +0400

    Update test_freq_dict.py
    В тесте я поняла, что нужно создать два метода, которые будут вызывать методы check_*, где в качетсве аргумента будет один из классов из основной программы. В результате нам не нужно описывать все эти методы для каждого класса отдельно, а только вызывать их с разными классами в качестве аргументов.
    Но у меня возник вопрос: как в данной ситуации задавать результаты выполнения методов из основной программы в assert (например в методе chek_sort() это sort_fd)? Ведь для одного класса результатом будет словарь, а в другом - список.

--- test_freq_dict.py
+++ test_freq_dict.py
@@ -8,38 +8,42 @@ class TestFreqDict(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_split(self):
-        text = ['Иван Анна', 'Иван - Анна', 'Иван,- Анна', 'Иван Анна дом ']
-        list_of_words = [['иван','анна'], ['иван','анна'], ['иван','анна'], ['иван','анна','дом']]
+    def check_split(self):
+        text = ['aa bb', 'aa - bb', 'aa,- bb', 'aa bb cc ']
+        list_of_words = [['aa','bb'], ['aa','bb'], ['aa','bb'], ['aa','bb','cc']]
         for (t,l) in zip(text, list_of_words):
             self.assertTrue(dictionary.split_to_words(t) == l, 'split to words test: [false]')
 
-    def test_find_dict(self):
+    def check_find_dict(self):
         word = 'dd'
-        fd1 ={'aa': 2, 'dd': 1, 'd': 4}
+        fd1 = {'aa': 2, 'dd': 1, 'd': 4}
         fd2 = {'aa': 2, 'cc': 1, 'd': 4}
         self.assertTrue(dictionary.find_in_dict(word, fd1) == True, 'find word in freq dict test1: [false]')
         self.assertTrue(dictionary.find_in_dict(word, fd2) == None, 'find word in freq dict test2: [false]')
 
-    def test_add_word(self):
+    def check_add_word(self):
         word = 'dd'
-        fd1 = {'aa': 1, 'dd': 2}
-        new_fd1 = {'aa': 1, 'dd': 3}
-        fd2 = {'aa': 1, 'bb': 3}
-        new_fd2 = {'aa': 1, 'bb': 3, 'dd': 1}
-        self.assertTrue(dictionary.add_word(word, fd1) == new_fd1, 'add word in freq dict test1: [false]')
-        self.assertTrue(dictionary.add_word(word, fd2) == new_fd2, 'add word in freq dict test2: [false]')
+        fd = [{'aa': 1, 'dd': 2}, {'aa': 1, 'bb': 3}]
+        new_fd = [{'aa': 1, 'dd': 3}, {'aa': 1, 'bb': 3, 'dd': 1}]
+        for (f,n) in zip(fd, new_fd):
+            self.assertTrue(dictionary.add_word(word, f) == n, 'add word in freq dict test1: [false]')
 
-    def test_create_dict(self):
+    def check_create_dict(self):
         words = ['aa', 'bb', 'cc', 'aa', 'bb', 'bb']
         fd = {'aa': 2, 'bb': 3, 'cc': 1}
         self.assertTrue(dictionary.create_dict(words) == fd, 'create dict test: [false]')
 
-    def test_sort(self):
+    def check_sort(self):
         fd = {'ff': 2, 'bb': 3, 'cc': 1, 'aa': 2}
         sort_fd = {'bb': 3, 'ff': 2, 'aa': 2, 'cc': 1}
         self.assertTrue(dictionary.sort(fd) == sort_fd, 'sort test: [false]')
 
+    def test_list_impl(self):
+
+
+    def test_dict_impl(self):
+
+
 if __name__ == '__main__':
-    dictionary = freq_dict.dict() 
+    dictionary = freq_dict.FrequencyDict_DictImpl() 
     unittest.main()
