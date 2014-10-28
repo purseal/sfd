540314b9e944c656fced1d771ea872d4c9a0b8be
Maria <korevmaria@gmail.com>
2014-10-28 06:32:22 +0300

    Update freq_dict.py

--- freq_dict.py
+++ freq_dict.py
@@ -9,81 +9,82 @@ import operator
 
 class FrequencyDict:
 
+    fd =[] 
+
     def split_to_words(self, text):
         text = text.lower()
         text = text.strip()
         text = re.sub('\d', '', text)
         words = re.split('\W*', text)
         return words
-   
 
-class FrequencyDict_ListImpl(FrequencyDict):
-'''Implementation of creating a frequency dict with lists'''
-
-    fd =[] 
-
-    def find_in_dict(self ,word, fd):
-        for i in range(len(fd)):
-            if word == fd[i][0]:
-                return i
-         return None
+    def find_in_dict(self, word):
+        pass
 
     def add_word(self, word, fd):
-        i = find_in_dict(word, fd)
-        if find_in_dict(word, fd) is None:
-            fd.append([word, 1])
-        else:
-            fd[i][1] +=1
-        return fd
+        pass
 
     def create_dict(self, words):
         for word in words:
-            self.find_in_dict(word, self.fd)
-            self.add_word(word, self.fd)
+            self.add_word(word)
+        return self.fd 
+
+    def sort(self): 
+        pass
+
+class FreqDictList(FrequencyDict):
+    ''' Implementation of creating a frequency dict with lists. '''
+
+    def find_in_dict(self, word):
+        for i in range(len(self.fd)):
+            if word == self.fd[i][0]:
+                return i
+        return 0
+
+    def add_word(self, word):
+        i = self.find_in_dict(word)
+        if self.find_in_dict(word) is 0:
+            self.fd.append([word, 1])
+        else:
+            self.fd[i][1] +=1
+        return self.fd
+ 
+    def sort(self):
+        self.fd = sorted(self.fd, key= lambda dict: dict[1], reverse=True)
         return self.fd
 
-    def sort(self, fd):
-        fd = sorted(fd, key= lambda dict: dict[1], reverse=True)
-        return fd
 
-
-class FrequencyDict_DictImpl(FrequencyDict):
-'''Implementation of creating a frequency dict with dictionaries'''
+class FreqDictDict(FrequencyDict):
+    ''' Implementation of creating a frequency dict with dictionaries. '''
 
     fd ={} 
 
-    def find_in_dict(self ,word, fd):
-        if word in fd:    
-            return True
-        return None
+    def find_in_dict(self, word):
+        if word in self.fd:    
+            return -1
+        return 0
 
-    def add_word(self, word, fd):
-        if self.find_in_dict(word, fd) is None:
-            fd[word] = 1
+    def add_word(self, word):
+        if self.find_in_dict(word) is 0:
+            self.fd[word] = 1
         else:
-            fd[word] +=1
-        return fd
-
-    def create_dict(self, words):
-        for word in words:
-            self.find_in_dict(word, self.fd)
-            self.add_word(word, self.fd)
+            self.fd[word] +=1
         return self.fd
 
-    def sort(self, fd):
-        sorted(fd.items(), key=operator.itemgetter(1), reverse=True)
-        return fd
+    def sort(self):
+        sorted(self.fd.items(), key=operator.itemgetter(1), reverse=True)
+        return [[key, item] for key, item in self.fd.items()]
 
 
 if __name__ == '__main__':
-    sfd = FrequencyDict_DictImpl()
+    sfd = FreqDictList()
     input_file_name = sys.argv[1]
     output_file_name = sys.argv[2]
     with open(input_file_name) as text_file:
         string_of_text = text_file.read()
         words = sfd.split_to_words(string_of_text)
         fd = sfd.create_dict (words)
-        fd = sfd.sort(fd)
+        fd = sfd.sort()
     with open(output_file_name, 'w+') as fd_file:
         for count, word in fd:
             output = '{} {}\n'.format(word, count)
