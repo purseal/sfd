c8320bbafe48956425cd5286371fdecbc3042e43
Maria <korevmaria@gmail.com>
2014-11-13 14:55:18 +0300

    some changes in class

--- test_tree_strucrute.py
+++ test_tree_strucrute.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
+import pdb
 import tree_structure as ts 
 import unittest
+import copy
 
 class TestTreeStruct(unittest.TestCase):
     word1 = 'b'
     word2 = 'c'
-    empty_tree = ts.Tree()
     root = ts.Node('d', 1, None, None)
     node1 = ts.Node('a', 1, None, None)
     node2 = ts.Node('b', 2, None, None)
@@ -17,30 +18,58 @@ class TestTreeStruct(unittest.TestCase):
     tree = ts.Tree()
     tree.root = root
     node4 = ts.Node('c', 1, None, None)
-    node2.right = node4
-    result_tree = root
+    exp_tree = ts.Tree()
+    root2 = ts.Node('d', 1, None, None)
+    root2.left = node2
+    root2.right = node3
+    root2.left.left = node1
+    exp_tree.root = root2
+    exp_tree.root.left.right = node4
+
+    def test_node_eq(self):
+        n1 = ts.Node('a', 2)
+        assert n1 == copy.copy(n1)
+        assert n1 != ts.Node('b', 2)
+        assert n1 != ts.Node('a', 3)
+        assert n1 != ts.Node('a', 2, ts.Node('a', 1))
+        assert ts.Node('a', 2, ts.Node('a', 1)) != n1
+        n1.left = ts.Node('a', 1)
+        assert ts.Node('a', 2, ts.Node('a', 1)) == n1
+        assert not n1 != n1
 
     def test_find_parent(self):
-        res = self.empty_tree.find_parent(self.word1)
-        self.assertTrue(res == None, 'test find parent: [false], was {}, should None'.format(res))
-        list_of_exp_parent = [self.node2, self.node2]
-        list_of_res = [self.tree.find_parent(self.word2), self.tree.find_parent(self.word1)]
-        for r,e in zip(list_of_exp_parent, list_of_res): 
-            self.assertTrue(r == e, '''test find parent: [false],
-                was: res({}, {},'''.format(r.word, r.cnt) + ' {}, {}), '.format( r.left, r.right) + '''
-                should: exp_tree({}, {},'''.format(e.word, e.cnt) + '{}, {})'.format(e.left, e.right))
-        print ('find_parent error')
+        empty_tree = ts.Tree()
+        res = empty_tree.find_parent(self.word1)
+        self.assertTrue(res == None, 'test find parent: [false], was {}, should None'.format(str(res)))
+        tree = ts.Tree()
+        tree.root = ts.Node('d', 1)
+        tree.root.left = ts.Node('b', 2)
+        tree.root.right = ts.Node('e', 1)
+        tree.root.left.left = ts.Node('a', 1)
+
+        exp_node = ts.Node('b', 2)
+        exp_node.left = ts.Node('a', 1)
+        res = tree.find_parent(self.word2)
+        message = 'test find parent: [false], was: res({}, {},'.format(
+            res.word, res.cnt)
+        message += ' {}, {}), '.format(res.left, res.right)
+        message += 'should: exp_tree({}, {},'.format(exp_node.word, exp_node.cnt) 
+        message += '{}, {})'.format(exp_node.left, exp_node.right)
+
+        assert res == exp_node, message
 
     def test_add_node(self):
+        empty_tree = ts.Tree()
+        self.tree.root = self.root
         e = ts.Tree()
         e.root = self.node2
-        list_of_exp_tree = [e, self.result_tree]
-        list_of_res = [self.empty_tree.add_node(self.word1), self.tree.add_node(self.word2)]
-        for r,e in zip(list_of_exp_tree, list_of_res): 
-            print('add_note error')
-            self.assertTrue(r == e, '''test find parent: [false],
-                was: res({}, {},'''.format(r.word, r.cnt) + ' {}, {}), '.format( r.left, r.right) + '''
-                should: exp_tree({}, {},'''.format(e.root.word, e.root.cnt) + '{}, {})'.format(e.left, e.right))
+        list_of_exp_tree = [e, self.exp_tree]
+        list_of_res = [empty_tree.add_node(self.word1), self.tree.add_node(self.word2)]
+        for e,r in zip(list_of_exp_tree, list_of_res): 
+            self.assertTrue(r == e, '''test add node: [false],
+                was: res({}, {},'''.format(r.root.word, r.root.cnt) + ' {}, {}), '.format( r.root.left, r.root.right) + '''
+                should: exp_tree({}, {},'''.format(e.root.word, e.root.cnt) + '{}, {})'.format(e.root.left, e.root.right))
     
+
 if __name__ == '__main__':
     unittest.main()
--- tree_structure.py
+++ tree_structure.py
@@ -6,6 +6,19 @@ class Node:
         self.cnt = cnt
         self.left = left
         self.right = right
+    
+    def __repr__(self):
+        return 'node: {} {}, l={}, r={}'.format(self.word, self.cnt,
+                                                None if not self.left else self.left.word,
+                                                None if not self.right else self.right.word)
+
+    def __eq__(self, other):
+        if not other:
+            return False
+        return (self.word == other.word
+                and self.cnt == other.cnt
+                and self.left == other.left
+                and self.right == other.right)
 
 class Tree:
     node = Node()
@@ -13,41 +26,78 @@ class Tree:
     def __init__(self):
         self.root = None
 
+    def __repr__(self):
+        if not self.root:
+            return 'None'
+        res = ''
+        res += str(self.root)
+        res += '\n'
+        res += '\t' + str(self.root.left)
+        res += '\t' + str(self.root.right)
+        return res
+
+    def __eq__(tree1, tree2):
+        node1 = Node()
+        node2 = Node()
+        list_nodes1 = [tree1.root]
+        list_nodes2 = [tree2.root]
+        while len(list_nodes1) != 0:
+            node1 = list_nodes1[0]
+            node2 = list_nodes2[0]
+            was_added = False
+            if ((node1.word == node2.word) and (node1.cnt == node2.cnt) and (node1.left == node2.left)):
+                list_nodes1.append(node1.left)
+                list_nodes2.append(node2.left)
+                was_added = True
+            if ((node1.word == node2.word) and (node1.cnt == node2.cnt) and (node1.right == node2.right)):
+                list_nodes1.append(node1.right)
+                list_nodes2.append(node2.right)
+                was_added = True
+            list_nodes1.remove(node1) 
+            list_nodes2.remove(node2) 
+            if was_added == False:
+                return False
+        return True
+
     def find_parent(self, word):
+        print('self.root == ', self)
         if self.root == None:
-            return self.root
+            return None
         parent = self.root
         i = 0
-        while ((parent.left != None) and (parent.right != None) and (i<6)):
-            print(i)
-            print(parent.word)
-            print(parent.left)
-            print(parent.right)
+        while parent.left != None or parent.right != None and i<6:
             if parent.word > word:
-                parent = parent.left
+                if parent.left is None:
+                    return parent
+                else:
+                    parent = parent.left
             elif parent.word < word:
-                parent = parent.right
+                if parent.right is None:
+                    return parent
+                else:
+                    parent = parent.right
+            else: 
+                return parent
             i += 1
-        return parent
+        return parent 
         
-    def add_node(self, word):
-        parent = self.find_parent(word)
-        if parent == None:
-            new_node = Node(None, None, None, None)
-            new_node.word = word
-            new_node.cnt = 1
-            return new_node
-        if word == parent.word:
-            parent.cnt += 1
-            return parent
+def add_node(self, word):
+    parent = self.find_parent(word)
+    if parent == None:
         new_node = Node(None, None, None, None)
         new_node.word = word
         new_node.cnt = 1
-        if parent.word > word:
-            parent.left = new_node
-            return new_node
-        root_node.right = new_node
-        return new_node
-
-if __name__ == '__main__':
-    pass        
+        self.root = new_node
+        return self
+    list_nodes = [self.root]
+    if word == parent.word:
+        parent.cnt += 1
+        return self
+    new_node = Node(None, None, None, None)
+    new_node.word = word
+    new_node.cnt = 1
+    if parent.word > word:
+        parent.left = new_node
+        return self
+    parent = new_node
+    return self 
