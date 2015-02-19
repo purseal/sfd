9dd5ce8b8841a9f3a0a27d479b7fded7870f6476
Maria <korevmaria@gmail.com>
2015-02-20 01:31:07 +0300

    Authentication failed

--- scts/send_comand_to_server.py
+++ scts/send_comand_to_server.py
@@ -3,16 +3,21 @@
     and transform its output in a readable view.
 '''
 
-import re
+import os
+import paramiko
 import os
 
 class DataGetter:
     ''' Abstract class for any data downloader. '''
 
     def get_data(self, string):
+        ''' Dowload all informayion from given ip address.
+            Return string value, containing downloaded data.
+        '''
         pass
 
     def parse_data(self, data):
+        ''' Parse given data. '''
         pass
 
 class SshDataGetter(DataGetter):
@@ -23,6 +28,7 @@ class SshDataGetter(DataGetter):
         self.username = ''
         self.ip = ''
         self.password = ''
+        self.data = ''
 
     def split_to_values(self, given_string):
         ''' Method splits a string to list of values. '''
@@ -45,5 +51,23 @@ class SshDataGetter(DataGetter):
         self.password = list_of_values[4]
         self.comand = list_of_values[5]
 
-    def get_data(self, address):
-        pass
+    def get_my_data(self):
+        my_file = open('/home/maria/prog/sfd/scts/my_data', 'r')
+        data = my_file.readlines()
+        self.ip = data[0]
+        self.username = data[1]
+        self.password = data[2]
+        my_file.close()
+
+
+    def get_data(self):
+        ''' Method connects to server and sends the comand. '''
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print ('before connection')
+        ssh.connect(self.ip, username=self.username, password=self.password)
+        print ('after connection')
+        stdin, stdout, stderr = ssh.exec_command(self.comand)
+        self.data = stdout.read()
+        ssh.close()
+        return self.data
