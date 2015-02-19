de15c46ce3335aa61cb4b6636d518fda5fc3e72a
Maria <korevmaria@gmail.com>
2015-02-20 01:31:55 +0300

    Authentication failed

--- scts/test_send_comand_to_server.py
+++ scts/test_send_comand_to_server.py
@@ -5,35 +5,49 @@ import unittest
 from send_comand_to_server import SshDataGetter as SDG
 
 class TestSShDataGetter(unittest.TestCase):
+    ''' Class of test. '''
     message = 'was: {}, should: {}'
-    string = "$telnet -c 'echo xx' username ip password"
-    words = ["$telnet", "-c", "username", "ip", "password", "echo xx"]
+    string = "telnet -c 'echo xx' username ip password"
+    words = ["telnet", "-c", "username", "ip", "password", "echo xx"]
+    my_ip = '127.0.0.1'
+    my_username = 'maria'
+    my_comand = 'echo it works!'
+    output_data = 'it works!'
 
     def test_split_to_values(self):
         ''' Method makes a test of split_to_values in
             send_comand_to_server.py.
         '''
-        SDG_object = SDG()
-        result = SDG_object.split_to_values(self.string)
+        sdg_object = SDG()
+        result = sdg_object.split_to_values(self.string)
         assert result == self.words, self.message.format(result, self.words)
 
     def test_get_values(self):
         ''' Method makes tests of get_values in send_comand_to_server.py. '''
-        SDG_object = SDG()
-        SDG_object.get_values(self.string)
-        assert SDG_object.username == self.words[2], self.message.format(
-            SDG_object.username, self.words[2]
+        sdg_object = SDG()
+        sdg_object.get_values(self.string)
+        assert sdg_object.username == self.words[2], self.message.format(
+            sdg_object.username, self.words[2]
             )
-        assert SDG_object.ip == self.words[3], self.message.format(
-            SDG_object.ip, self.words[3]
+        assert sdg_object.ip == self.words[3], self.message.format(
+            sdg_object.ip, self.words[3]
             )
-        assert SDG_object.password == self.words[4], self.message.format(
-            SDG_object.password, self.words[4]
+        assert sdg_object.password == self.words[4], self.message.format(
+            sdg_object.password, self.words[4]
             )
-        assert SDG_object.comand == self.words[5], self.message.format(
-            SDG_object.comand, self.words[5]
+        assert sdg_object.comand == self.words[5], self.message.format(
+            sdg_object.comand, self.words[5]
             )
 
+    def test(self):
+        sdg_object = SDG()
+        sdg_object.comand = self.my_comand
+        sdg_object.get_my_data()
+        result = sdg_object.get_data()
+
+        assert result == self.output_data, self.message.format(
+            result, self.output_data
+            )
 
 if __name__ == '__main__':
     unittest.main()
