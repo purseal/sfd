2fd55fdeb7592bf3197cdfd2aa578e09e58d54a6
Maria <korevmaria@gmail.com>
2015-03-02 22:45:49 +0300

    Need a help

--- scts/send_comand_to_server.py
+++ scts/send_comand_to_server.py
@@ -3,9 +3,10 @@
     and transform its output in a readable view.
 '''
 
-import os
+from subprocess import Popen, PIPE
 import paramiko
 import os
+import sys
 
 class DataGetter:
     ''' Abstract class for any data downloader. '''
@@ -52,7 +53,7 @@ class SshDataGetter(DataGetter):
         self.comand = list_of_values[5]
 
     def get_my_data(self):
-        my_file = open('/home/maria/prog/sfd/scts/my_data', 'r')
+        my_file = open('/home/maria/prog/my_data', 'r')
         data = my_file.readlines()
         self.ip = data[0]
         self.username = data[1]
@@ -62,12 +63,39 @@ class SshDataGetter(DataGetter):
 
     def get_data(self):
         ''' Method connects to server and sends the comand. '''
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        print ('before connection')
-        ssh.connect(self.ip, username=self.username, password=self.password)
-        print ('after connection')
-        stdin, stdout, stderr = ssh.exec_command(self.comand)
-        self.data = stdout.read()
-        ssh.close()
+     #этот способ выдает ошибку идентификации
+     #File "/usr/local/lib/python3.4/dist-packages/paramiko/transport.py", line 1168, in auth_password
+     #return self.auth_handler.wait_for_response(my_event)
+     #File "/usr/local/lib/python3.4/dist-packages/paramiko/auth_handler.py", line 208, in wait_for_response
+     #raise e
+     #paramiko.ssh_exception.AuthenticationException: Authentication failed.
+
+     #   ssh = paramiko.SSHClient()
+     #   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+     #   print ('before connection')
+     #   ssh.connect(self.ip, username=self.username,
+     #       password=self.password, look_for_keys=False
+     #       )
+     #   print ('after connection')
+     #   stdin, stdout, stderr = ssh.exec_command(self.comand)
+     #   self.data = stdout.read()
+     #   ssh.close()
+
+#        os.system('ssh ' + self.ip)
+#        sys.stdin(bytes(self.password))
+#        os.system(self.password)
+#        proc = subprocess.Popen(self.command, stdout=subprocess.PIPE)
+#        self.data = proc.stdout.read()
+
+        #здесь выходит ошибка Permission denied (publickey,password).
+        proc = Popen(['ssh', self.ip], stdin=PIPE)
+        print('after ssh 127.0.0.1')
+        proc.stdin.write(self.password)
+        proc.stdin.flush()
+        print('after stdin.write')
+        proc = Popen(self.comand, stdout=PIPE)
+        proc.wait()
+        print('after command')
+        self.data = proc.stdout.read()
+        print('after stdout')
         return self.data
